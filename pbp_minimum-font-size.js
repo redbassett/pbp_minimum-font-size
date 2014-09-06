d5f9c257bde94a22d443cadec4a76b3957d9fb69
Harry Grillo <redbassett@gmail.com>
2014-09-05 21:54:20 -0400

    Adds callback for pagination changes to fix #2

--- pbp_minimum-font-size.js
+++ pbp_minimum-font-size.js
@@ -1,17 +1,23 @@
 $(function(){
 	settings = pb.plugin.get('minimum_message_font_size').settings;
-	if (($.inArray(0,settings.posts_or_pms) && pb.data('route').name == 'thread') || ($.inArray(1,settings.posts_or_pms) && pb.data('route').name == 'conversation')) {$('div.message:not(.ui-resizable)').each(function(){
-		if ($(this).css('font-size').replace('px','') < settings.minimum_size) {
-			$(this).css('font-size',settings.minimum_size+'px');
-			if (settings.override_size_tags > 0) {
-				$(this).find('font[size]').each(function(){
-					if ($(this).css('font-size').replace('px','') < settings.minimum_size) {
-						$(this).css('font-size',settings.minimum_size+'px');
+	if (($.inArray(0,settings.posts_or_pms) && pb.data('route').name == 'thread') || ($.inArray(1,settings.posts_or_pms) && pb.data('route').name == 'conversation')) {
+		function fixSizing() {
+			$('div.message:not(.ui-resizable)').each(function(){
+				if ($(this).css('font-size').replace('px','') < settings.minimum_size) {
+					$(this).css('font-size',settings.minimum_size+'px');
+					if (settings.override_size_tags > 0) {
+						$(this).find('font[size]').each(function(){
+							if ($(this).css('font-size').replace('px','') < settings.minimum_size) {
+								$(this).css('font-size',settings.minimum_size+'px');
+							}
+						});
 					}
-				});
-			}
+				}
+			});
 		}
-	});
-}
-}
+		fixSizing();
+		proboards.on('pageChange',function(){
+			fixSizing();
+		});
+	}
 });
