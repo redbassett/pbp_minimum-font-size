9d7c4a7b6f9e574cc5ee630649c327fe19aad2cd
Harry Grillo <redbassett@gmail.com>
2014-09-05 21:52:48 -0400

    Updated to add settings variable rather than referencing settings call every time.

--- pbp_minimum-font-size.js
+++ pbp_minimum-font-size.js
@@ -1,16 +1,17 @@
 $(function(){
-	if (($.inArray(0,pb.plugin.get('minimum_message_font_size').settings.posts_or_pms) && pb.data('route').name == 'thread') || ($.inArray(1,pb.plugin.get('minimum_message_font_size').settings.posts_or_pms) && pb.data('route').name == 'conversation')) {
-		$('div.message:not(.ui-resizable)').each(function(){
-			if ($(this).css('font-size').replace('px','') < pb.plugin.get('minimum_message_font_size').settings.minimum_size) {
-				$(this).css('font-size',pb.plugin.get('minimum_message_font_size').settings.minimum_size+'px');
-				if (pb.plugin.get('minimum_message_font_size').settings.override_size_tags > 0) {
-					$(this).find('font[size]').each(function(){
-						if ($(this).css('font-size').replace('px','') < pb.plugin.get('minimum_message_font_size').settings.minimum_size) {
-							$(this).css('font-size',pb.plugin.get('minimum_message_font_size').settings.minimum_size+'px');
-						}
-					});
-				}
+	settings = pb.plugin.get('minimum_message_font_size').settings;
+	if (($.inArray(0,settings.posts_or_pms) && pb.data('route').name == 'thread') || ($.inArray(1,settings.posts_or_pms) && pb.data('route').name == 'conversation')) {$('div.message:not(.ui-resizable)').each(function(){
+		if ($(this).css('font-size').replace('px','') < settings.minimum_size) {
+			$(this).css('font-size',settings.minimum_size+'px');
+			if (settings.override_size_tags > 0) {
+				$(this).find('font[size]').each(function(){
+					if ($(this).css('font-size').replace('px','') < settings.minimum_size) {
+						$(this).css('font-size',settings.minimum_size+'px');
+					}
+				});
 			}
-		});
-	}
+		}
+	});
+}
+}
 });
